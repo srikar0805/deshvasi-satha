500cf4dcd74821e1f69ab95dd9032677cb4c321e
srikar <saisrikarreddy.k20@iiits.in>
2022-06-30 10:19:07 +0530

    formatting home

---
+++ home page/home.js
@@ -0,0 +1,13 @@
+var myString = "Welcome to Desh Vasi Satha.";
+var myArray = myString.split("");
+var loopTimer;
+
+function looper() {
+  if(myArray.length > 0){
+    document.getElementById("typingText").innerHTML += myArray.shift();
+  } else {
+    clearTimeout(loopTimer);
+  }
+  loopTimer = setTimeout('looper()', 100);
+}
+looper();
