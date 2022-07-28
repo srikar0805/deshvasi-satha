0379a07e30496912e81a149598cd04e328e57f21
srikar <saisrikarreddy.k20@iiits.in>
2022-07-28 10:49:18 +0530

    something

--- node_js/server.js
+++ node_js/server.js
@@ -1,6 +1,10 @@
+const { response } = require('express');
 const mysql = require("mysql2");
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require('path');
+const { render } = require('express/lib/response');
+const { nextTick } = require('process');
 var app = express();
 app.use(bodyParser.json());
 
@@ -20,6 +24,12 @@ mysqlConnection.connect((err) => {
     }
 })
 
+app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
 
 
 
