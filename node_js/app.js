a604105c949b620bf05370affcd94d9bde632d20
srikar <saisrikarreddy.k20@iiits.in>
2022-12-19 22:19:21 +0530

    slight name and server changes

--- node_js/app.js
+++ node_js/app.js
@@ -22,10 +22,36 @@ mysqlConnection.connect((err) => {
     } else {
         console.log("Connection Failed");
     }
+    mysqlConnection.query("CREATE DATABASE IF NOT EXISTS webproject", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+    });
+    var sql = "CREATE TABLE IF NOT EXISTS voter (voter_id INT AUTO_INCREMENT PRIMARY KEY, voter_name VARCHAR(255), voter_password VARCHAR(25), voter_id VARCHAR(255) NOT NULL UNIQUE, voter_wardnumber VARCHAR(5), voter_constituency VARCHAR(255), voter_mandal VARCHAR(255), voter_district VARCHAR(255), voter_state VARCHAR(255), voter_phone VARCHAR(10), voter_dob DATE)";
+    mysqlConnection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Voter Table created");
+    });
+    var sql = "CREATE TABLE IF NOT EXISTS party_worker (pw_id INT AUTO_INCREMENT PRIMARY KEY, pw_name VARCHAR(255), pw_username VARCHAR(255), pw_id VARCHAR(255) NOT NULL UNIQUE, pw_password VARCHAR(25), pw_wardnumber VARCHAR(5), pw_constituency VARCHAR(255), pw_mandal VARCHAR(255), pw_district VARCHAR(255), pw_state VARCHAR(255), pw_phone VARCHAR(10), pw_dob DATE)";
+    mysqlConnection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Party Worker Table created");
+    });
+    var sql = "CREATE TABLE IF NOT EXISTS party_worker_admin (pw_admin_id INT AUTO_INCREMENT PRIMARY KEY, pw_admin_name VARCHAR(255), pw_admin_username VARCHAR(255), pw_admin_id VARCHAR(255) NOT NULL UNIQUE, pw_admin_password VARCHAR(25), pw_admin_phone VARCHAR(10), pw_admin_dob DATE)";
+    mysqlConnection.query(sql, function
+        (err, result) {
+        if (err) throw err;
+        console.log("Party Worker Admin Table created");
+    });
+    
+
 })
+
+
 
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
+
+function CreateTable()
 
 app.get("/", (req, res) => {
     res.render("home")
@@ -47,25 +73,66 @@ app.get("/partyworkeradminlogin", (req, res) => {
     res.render("pwadminlogin")
 })
 
-app.get("/partyworker", (req, res) => {
-    res.render("pwhome")
-})
+
+
+// app.get("/partyworker", (req, res) => {
+//     res.render("pwhome")
+// })
 
 app.get("/register", (req, res) => {
     res.render("register")
 })
 
-app.get("/voter", (req, res) => {
-    res.render("voterhome")
+app.post("/register", (req, res) => {
+    var sql = "INSERT INTO voter (voter_name, voter_password, voter_id, voter_wardnumber, voter_constituency, voter_mandal, voter_district, voter_state, voter_phone, voter_dob) VALUES ('" + req.body.username + "', '" + req.body.password + "', '" + req.body.voter_id + "', '" + req.body.ward + "', '" + req.body.constituency + "', '" + req.body.mandal + "', '" + req.body.dist + "', '" + req.body.state + "', '" + req.body.phone + "', '" + req.body.dob + "')";
+    mysqlConnection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
+    res.redirect("/voterlogin");
 })
 
-app.get("/workeradminhome", (req, res) => {
-    res.render("workeradminhome")
+app.post("/voterlogin", (req, res) => {
+    var sql = "SELECT * FROM voter WHERE voter_id = '" + req.body.voter_id + "' AND voter_password = '" + req.body.password + "'";
+    mysqlConnection.query(sql, function
+        (err, result) {
+        if (err) throw err;
+        if (result.length > 0) {
+            res.render("/voterhome", { result: result });
+        }
+        else {
+            res.redirect("/voterlogin");
+        }
+    });
 })
 
-app.get("/voteradminhome", (req, res) => {
-    res.render("voteradminhome")
+app.post("/partyworkerlogin", (req, res) => {
+    var sql = "SELECT * FROM party_worker WHERE pw_username = '" + req.body.username + "' AND pw_password = '" + req.body.password + "'";
+    mysqlConnection.query(sql, function
+        (err, result) {
+        if (err) throw err;
+        if (result.length > 0) {
+            res.render("/pwhome", { result: result });
+        }
+        else {
+            res.redirect("/partyworkerlogin");
+        }
+    });
 })
+
+
+
+// app.get("/voter", (req, res) => {
+//     res.render("voterhome")
+// })
+
+// app.get("/workeradminhome", (req, res) => {
+//     res.render("workeradminhome")
+// })
+
+// app.get("/voteradminhome", (req, res) => {
+//     res.render("voteradminhome")
+// })
 
 
 
